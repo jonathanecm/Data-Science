464635a6c51351bd07a039c2c701248769c4f249
Jonathan <jonathanecm@icloud.com>
2016-06-09 15:51:39 -0400

    Adding "Forms of the Extract Operator in R" file

---
+++ R Programming/Week 1/Forms of the Extract Operator in R.R
@@ -0,0 +1,8 @@
+## Forms of the Extract Operator in R
+
+# Extractor ([]) used to extract content from vectors, lists, or data frames. 
+## Using [] with direct refering of elemets.
+
+normal <- 16:30 # vector definition
+
+## Using [] with the length of the object to calculate the indexes
